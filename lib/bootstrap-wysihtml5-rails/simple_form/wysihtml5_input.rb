5cdf9cd080441aaa8c1a75fc74376cda12e8017d
Marion Duprey <marion.duprey@bookandgolf.com>
2016-03-08 11:22:30 +0100

    fix js issue when jquery imported at end of page

--- lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
+++ lib/bootstrap-wysihtml5-rails/simple_form/wysihtml5_input.rb
@@ -2,7 +2,9 @@ class Wysihtml5Input < SimpleForm::Inputs::TextInput
   def input(wrapper_options)
     idf = "#{lookup_model_names.join("_")}_#{reflection_or_attribute_name}"
     script = template.content_tag(:script, type: 'text/javascript') do
-      "$('textarea[id=#{idf}]').wysihtml5(#{options[:wysihtml5].to_json});".html_safe
+      "$(document).ready(function() {
+        $('textarea[id=#{idf}]').wysihtml5(#{options[:wysihtml5].to_json});
+      });".html_safe
     end
 
     super + script
